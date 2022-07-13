5ea387278d4a06c8d028b1777c68e1b40309f5c2
Andrew2000-jS <andresarevalo0609@gmail.com>
2022-07-13 17:18:35 -0400

    [Add] update and delete method

--- contracts/PostFactory.sol
+++ contracts/PostFactory.sol
@@ -15,6 +15,8 @@ contract PostFactory is Ownable {
     }
 
     event CreatedPost(string description, uint256 id, address owner);
+    event UpdatedPost(address owner, string udpatedDescription);
+    event DeletedPost(uint256 id, string);
 
     function createPost(string memory _description) public {
         require(
@@ -24,6 +26,34 @@ contract PostFactory is Ownable {
 
         postCounter++;
         posts[msg.sender] = Post(_description, postCounter, msg.sender);
+        postOwners[postCounter] = msg.sender;
         emit CreatedPost(_description, postCounter, msg.sender);
+    }
+
+    function updatePost(uint256 _id, string memory _description)
+        public
+        onlyOwner
+    {
+        address ownerFound = _findOwner(_id);
+        Post storage updatedPost = posts[ownerFound];
+        updatedPost.description = _description;
+
+        emit UpdatedPost(ownerFound, _description);
+    }
+
+    function deletePost(uint256 _id) public {
+        address postToDelete = _findOwner(_id);
+        delete posts[postToDelete];
+
+        emit DeletedPost(_id, "Deleted successfully");
+    }
+
+    function _findOwner(uint256 _id) private view returns (address) {
+        address ownerAddress = postOwners[_id];
+        if (ownerAddress != msg.sender && ownerAddress == address(0x0)) {
+            revert("Not found");
+        }
+
+        return ownerAddress;
     }
 }
--- test/PostFactory.test.js
+++ test/PostFactory.test.js
@@ -17,7 +17,7 @@ contract("PostFactory", () => {
 
   describe("Actions", async () => {
     it("Should can create a post", async function () {
-      const address = "0x9460F9fd096031C833e51f9632361eC606224169";
+      const address = "0xb81CB1D7b001253F221dEbfC0bDDfb3A98895379";
 
       const createPost = await this.postFactory.createPost("test description");
       const posts = await this.postFactory.posts(address);
@@ -31,6 +31,33 @@ contract("PostFactory", () => {
       assert.equal(posts.description, "test description");
       assert.equal(posts.id.toNumber(), 1);
       assert.equal(posts.owner, address);
+    });
+
+    it("Should can update a post", async function () {
+      const address = "0xb81CB1D7b001253F221dEbfC0bDDfb3A98895379";
+      const createPost = await this.postFactory.createPost(
+        "I want to be udpate"
+      );
+
+      assert.equal(createPost.logs[0].args[2], address);
+      assert.equal(createPost.logs[0].args[0], "I want to be udpate");
+
+      const updatePost = await this.postFactory.updatePost(1, "Updated task");
+      assert.equal(updatePost.logs[0].event, "UpdatedPost");
+      assert.equal(updatePost.logs[0].args[0], address);
+      assert.equal(updatePost.logs[0].args[1], "Updated task");
+    });
+
+    it("Should can delete a post", async function () {
+      const address = "0xb81CB1D7b001253F221dEbfC0bDDfb3A98895379";
+      await this.postFactory.createPost("test description");
+
+      const deletePost = await this.postFactory.deletePost(1);
+      const posts = await this.postFactory.posts(address);
+
+      assert.equal(deletePost.logs[0].event, "DeletedPost");
+      assert.equal(deletePost.logs[0].args[1], "Deleted successfully");
+      assert.equal(posts.description, "");
     });
   });
 });
