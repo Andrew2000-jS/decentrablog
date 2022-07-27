53cecd93f7c52276a4ee0ea20cbaa8f9a326c565
Andrew2000-jS <andresarevalo0609@gmail.com>
2022-07-27 16:47:16 -0400

    [Add] new features

--- contracts/PostFactory.sol
+++ contracts/PostFactory.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract PostFactory is Ownable {
+contract PostFactory is Ownable, ERC721URIStorage {
     uint256 public postCounter;
+    uint256 public tokenCount;
 
-    mapping(address => Post) public posts;
-    mapping(uint256 => address) public postOwners;
+    mapping(uint256 => Post) public posts;
+    mapping(address => uint256) public postOwners;
+    mapping(uint256 => ProcfileNft) public profile;
 
     struct Post {
         string description;
@@ -14,19 +17,69 @@ contract PostFactory is Ownable {
         address owner;
     }
 
+    struct ProcfileNft {
+        address payable owner;
+        uint256 id;
+        uint256 price;
+    }
+
     event CreatedPost(string description, uint256 id, address owner);
-    event UpdatedPost(address owner, string udpatedDescription);
+    event UpdatedPost(uint256 id, string udpatedDescription);
     event DeletedPost(uint256 id, string);
+    event MintedNFT(uint256 tokenId, address owner);
+    event TransferNFT(address _seller, address _buyer, uint256 price);
+
+    constructor() ERC721("Decentrablog", "DCB") {}
+
+    function mint(string memory _tokenURI, uint256 _price)
+        external
+        returns (bool)
+    {
+        tokenCount++;
+        ProcfileNft memory _nft;
+        _nft.price = _price;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        setProfile(tokenCount, _price);
+        emit MintedNFT(tokenCount, msg.sender);
+        return true;
+    }
+
+    function setProfile(uint256 _id, uint256 _price) public {
+        require(
+            ownerOf(_id) == msg.sender,
+            "Must own the nft you want to select as your profile"
+        );
+
+        postOwners[msg.sender] = _id;
+        uint256 id = postOwners[msg.sender];
+        profile[id] = ProcfileNft(payable(msg.sender), id, _price);
+    }
+
+    function getPostAll() public view returns (Post[] memory _posts) {
+        _posts = new Post[](postCounter);
+        for (uint256 i = 0; i < _posts.length; i++) {
+            _posts[i] = posts[i + 1];
+        }
+    }
+
+    function getAllNfts() external view returns (ProcfileNft[] memory _nfts) {
+        _nfts = new ProcfileNft[](tokenCount);
+
+        for (uint256 i = 0; i < _nfts.length; i++) {
+            _nfts[i] = profile[i + 1];
+        }
+    }
 
     function createPost(string memory _description) public {
         require(
-            keccak256(abi.encodePacked(_description)) != "",
+            keccak256(abi.encodePacked(_description)) !=
+                keccak256(abi.encodePacked("")),
             "Please enter a valid description"
         );
 
         postCounter++;
-        posts[msg.sender] = Post(_description, postCounter, msg.sender);
-        postOwners[postCounter] = msg.sender;
+        posts[postCounter] = Post(_description, postCounter, msg.sender);
         emit CreatedPost(_description, postCounter, msg.sender);
     }
 
@@ -34,26 +87,29 @@ contract PostFactory is Ownable {
         public
         onlyOwner
     {
-        address ownerFound = _findOwner(_id);
-        Post storage updatedPost = posts[ownerFound];
+        require(
+            keccak256(abi.encodePacked(_description)) !=
+                keccak256(abi.encodePacked("")),
+            "Please enter a valid description"
+        );
+        uint256 postId = _findPost(_id);
+        Post storage updatedPost = posts[postId];
         updatedPost.description = _description;
-
-        emit UpdatedPost(ownerFound, _description);
+        emit UpdatedPost(postId, _description);
     }
 
     function deletePost(uint256 _id) public {
-        address postToDelete = _findOwner(_id);
+        uint256 postToDelete = _findPost(_id);
         delete posts[postToDelete];
-
         emit DeletedPost(_id, "Deleted successfully");
     }
 
-    function _findOwner(uint256 _id) private view returns (address) {
-        address ownerAddress = postOwners[_id];
-        if (ownerAddress != msg.sender && ownerAddress == address(0x0)) {
-            revert("Not found");
-        }
+    function _findPost(uint256 _id) private view returns (uint256) {
+        Post memory post = posts[_id];
 
-        return ownerAddress;
+        if (post.id == _id) {
+            return post.id;
+        }
+        revert("Not found");
     }
 }
--- test/PostFactory.test.js
+++ test/PostFactory.test.js
@@ -1,63 +1,129 @@
 const PostFactory = artifacts.require("PostFactory");
+const { expect, use } = require("chai");
+const { solidity } = require("ethereum-waffle");
 
-contract("PostFactory", () => {
+use(solidity);
+
+contract("PostFactory", (accounts) => {
+  let [alice, bob] = accounts;
   before(async function () {
     this.postFactory = await PostFactory.deployed();
   });
 
   describe("Deployment", async () => {
-    it("migrate deployed successfully", async function () {
+    xit("migrate deployed successfully", async function () {
       const address = this.postFactory.address;
-      assert.notEqual(address, null);
-      assert.notEqual(address, 0x0);
-      assert.notEqual(address, undefined);
-      assert.notEqual(address, "");
+      expect(address).to.not.equal(null);
+      expect(address).to.not.equal(0x0);
+      expect(address).to.not.equal(undefined);
+      expect(address).to.not.equal("");
     });
   });
 
-  describe("Actions", async () => {
-    it("Should can create a post", async function () {
-      const address = "0xb81CB1D7b001253F221dEbfC0bDDfb3A98895379";
+  describe("Features", async () => {
+    xit("Should can get all posts", async function () {
+      await this.postFactory.createPost("test description");
+      await this.postFactory.createPost("test description 2");
+      await this.postFactory.createPost("test description 3");
 
-      const createPost = await this.postFactory.createPost("test description");
-      const posts = await this.postFactory.posts(address);
+      const postArr = await this.postFactory.getPostAll();
 
-      assert.equal(createPost.logs[0].event, "CreatedPost");
-      assert.equal(createPost.logs[0].args[0], "test description");
-      assert.equal(createPost.logs[0].args[1].toNumber(), 1);
-      assert.equal(createPost.logs[0].args[2], address);
+      expect(postArr.length).to.equal(3);
+      expect(postArr[0].description).to.equal("test description");
+      expect(postArr[1].description).to.equal("test description 2");
+      expect(postArr[2].description).to.equal("test description 3");
+    });
+
+    xit("Should can create a post", async function () {
+      const createPost = await this.postFactory.createPost("test description", {
+        from: alice,
+      });
+      const posts = await this.postFactory.posts(1);
+
+      expect(createPost.logs[0].event).to.equal("CreatedPost");
+      expect(createPost.logs[0].args[0]).to.equal("test description");
+      expect(createPost.logs[0].args[1].toNumber()).to.equal(1);
+      expect(createPost.logs[0].args[2]).to.equal(alice);
 
       //check posts mapping
-      assert.equal(posts.description, "test description");
-      assert.equal(posts.id.toNumber(), 1);
-      assert.equal(posts.owner, address);
+      expect(posts.description).to.equal("test description");
+      expect(posts.id.toNumber()).to.equal(1);
+      expect(posts.owner).to.equal(alice);
     });
 
-    it("Should can update a post", async function () {
-      const address = "0xb81CB1D7b001253F221dEbfC0bDDfb3A98895379";
+    xit("Should can update a post", async function () {
       const createPost = await this.postFactory.createPost(
-        "I want to be udpate"
+        "I want to be udpate",
+        { from: bob }
       );
 
-      assert.equal(createPost.logs[0].args[2], address);
-      assert.equal(createPost.logs[0].args[0], "I want to be udpate");
+      expect(createPost.logs[0].args[0]).to.equal("I want to be udpate");
 
       const updatePost = await this.postFactory.updatePost(1, "Updated task");
-      assert.equal(updatePost.logs[0].event, "UpdatedPost");
-      assert.equal(updatePost.logs[0].args[0], address);
-      assert.equal(updatePost.logs[0].args[1], "Updated task");
+      expect(updatePost.logs[0].event).to.equal("UpdatedPost");
+      expect(updatePost.logs[0].args[1]).to.equal("Updated task");
     });
 
-    it("Should can delete a post", async function () {
-      const address = "0xb81CB1D7b001253F221dEbfC0bDDfb3A98895379";
+    xit("Should can delete a post", async function () {
       await this.postFactory.createPost("test description");
 
       const deletePost = await this.postFactory.deletePost(1);
-      const posts = await this.postFactory.posts(address);
 
-      assert.equal(deletePost.logs[0].event, "DeletedPost");
-      assert.equal(deletePost.logs[0].args[1], "Deleted successfully");
-      assert.equal(posts.description, "");
+      expect(deletePost.logs[0].event).to.equal("DeletedPost");
+      expect(deletePost.logs[0].args[1]).to.equal("Deleted successfully");
+    });
+
+    xit("Should can mint a nft", async function () {
+      await this.postFactory.mint("SampleURI", 5, { from: bob });
+
+      const tokenCount = await this.postFactory.tokenCount();
+      const postOwnersBob = await this.postFactory.postOwners(bob);
+      const postOwnersAlice = await this.postFactory.postOwners(alice);
+      const profile = await this.postFactory.profile(1);
+
+      expect(tokenCount.toNumber()).to.equal(1);
+      expect(postOwnersBob.toNumber()).to.equal(1);
+      expect(profile.owner).to.equal(bob);
+      expect(profile.id.toNumber()).to.equal(1);
+      expect(profile.price.toNumber()).to.equal(5);
+
+      expect(postOwnersAlice.toNumber()).to.equal(0);
+    });
+
+    xit("Should can get all nfts", async function () {
+      await this.postFactory.mint("SampleURI", 20, { from: bob });
+      await this.postFactory.mint("SampleURI", 15, { from: alice });
+
+      const getNFTS = await this.postFactory.getAllNfts();
+      const [nft1, nft2] = getNFTS;
+
+      expect(getNFTS.length).to.equal(2);
+
+      expect(Number(nft1.id)).to.equal(1);
+      expect(nft1.owner).to.equal(bob);
+      expect(Number(nft1.price)).to.equal(20);
+
+      expect(Number(nft2.id)).to.equal(2);
+      expect(nft2.owner).to.equal(alice);
+      expect(Number(nft2.price)).to.equal(15);
+    });
+  });
+
+  describe("failure cases", async () => {
+    xit("Should not be able to create a post", async function () {
+      await expect(this.postFactory.createPost("")).to.be.revertedWith(
+        "Please enter a valid description"
+      );
+    });
+
+    it("Should not be able to update a post", async function () {
+      const post = await this.postFactory.createPost("Test");
+
+      await expect(this.postFactory.updatePost(1, "")).to.be.revertedWith(
+        "Please enter a valid description"
+      );
+      // should keep the same title
+      expect(post.logs[0].args[0]).to.equal("Test");
     });
   });
 });
